6b5616120ed76b221119874a5435f3bf160cdef9
Aidan-Pick <ap4223@ic.ac.uk>
2025-08-03 17:46:26 +0100

    feat (task 2): Implemented TransactionConsumer and KafkaConsumerConfig, updated application.yml

---
+++ src/main/java/com/jpmc/midascore/component/TransactionConsumer.java
@@ -0,0 +1,18 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionConsumer {
+    @KafkaListener(
+            topics = "${general.kafka-topic}",
+            groupId = "midas-core-group",
+            containerFactory = "kafkaListenerContainerFactory"
+
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("Received transaction: " + transaction);
+    }
+}
